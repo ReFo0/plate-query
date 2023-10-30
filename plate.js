7224839c8da78a822358f7e38c79e319bb618f04
Emir <77904942+ReFo0@users.noreply.github.com>
2023-10-30 18:01:05 +0300

    Update plate.js

--- plate.js
+++ plate.js
@@ -1,1 +1,21 @@
+const axios = require('axios');
+const API_KEY = '8579a4168c00e09d615ff56c06ff73913c16708f'; //  Recognizer API Anhtarı
+const IMAGE_URL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfZOyq_QjHE09MOnaAZbCVqZ1sWoFZxlp7oQ';  // Plaka Fotoğrafı
 
+const config = {
+  headers: {
+    Authorization: `Token ${API_KEY}`,
+  },
+};
+
+axios
+  .get(`https://api.platerecognizer.com/v1/plate-reader?image=${IMAGE_URL}`, config)
+  .then((response) => {
+    const plateData = response.data; // plaka datası
+    if (plateData){
+    console.log("plate found.");
+    }
+    else if (!plateData){
+        console.log("plate not found.");
+    }
+  })
